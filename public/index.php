20b6c3dce130edc7e382bed5401254736949ff15
dawood-faiz-tvs <dawood.faiz.tvs@gmail.com>
2024-01-23 17:19:55 +0500

    Route and router files
    Separated the router and route files

--- public/index.php
+++ public/index.php
@@ -1,15 +1,6 @@
 <?php
 require('../helpers.php');
 
-$routes = [
-    '/' => 'controllers/home.php',
-    '/listings' => 'controllers/listings/index.php',
-    '/listings/create' => 'controllers/listings/create.php',
-    '404' => 'controllers/error/404.php',
-];
+$uri = $_SERVER['REQUEST_URI'];
 
-if (array_key_exists($_SERVER['REQUEST_URI'], $routes)) {
-    require(basePath($routes[$_SERVER['REQUEST_URI']]));
-} else {
-    require(basePath($routes['404']));
-}
+require(basePath('router.php'));
