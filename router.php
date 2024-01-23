08fbf2271e3cf13608e3b6772e5ec56420f7c146
dawood-faiz-tvs <dawood.faiz.tvs@gmail.com>
2024-01-23 19:41:31 +0500

    Router refactoring
    Created a custom controller which registers all the routes and then loads correct view based on the URI and request method

--- router.php
+++ router.php
@@ -1,9 +1,53 @@
 <?php
-$routes = require(basePath('routes.php'));
+class Router
+{
+    protected $routes = [];
 
-if (array_key_exists($uri, $routes)) {
-    require(basePath($routes[$uri]));
-} else {
-    http_response_code(404);
-    require(basePath($routes['404']));
+    public function resiterRoute($method, $uri, $controller)
+    {
+        $this->routes[] = [
+            'method' => $method,
+            'uri' => $uri,
+            'controller' => $controller
+        ];
+    }
+
+    public function get($uri, $controller)
+    {
+        $this->resiterRoute('GET', $uri, $controller);
+    }
+
+    public function post($uri, $controller)
+    {
+        $this->resiterRoute('POST', $uri, $controller);
+    }
+
+    public function put($uri, $controller)
+    {
+        $this->resiterRoute('PUT', $uri, $controller);
+    }
+
+    public function delete($uri, $controller)
+    {
+        $this->resiterRoute('DELETE', $uri, $controller);
+    }
+
+    public function error($errorCode = 404)
+    {
+        http_response_code($errorCode);
+        loadView("error/{$errorCode}");
+        exit;
+    }
+
+    public function route($uri, $method)
+    {
+        foreach ($this->routes as $route) {
+            if (strtolower($route['uri']) === $uri && $route['method'] === $method) {
+                require(basePath($route['controller']));
+                return;
+            }
+        }
+
+        $this->error();
+    }
 }
