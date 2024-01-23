44ac22d72533cb71ff442885ed0475ec1ce9f0a7
dawood-faiz-tvs <dawood.faiz.tvs@gmail.com>
2024-01-23 14:32:39 +0500

    Basic routing and .htaccess
    Added .htaccess that directs all URIs to index.php, Added basic routing functionality

--- public/index.php
+++ public/index.php
@@ -1,3 +1,16 @@
 <?php
 require('../helpers.php');
-loadView("home");
+// loadView("home");
+
+$routes = [
+    '/' => 'controllers/home.php',
+    '/listings' => 'controllers/listings/index.php',
+    '/listings/create' => 'controllers/listings/create.php',
+    '404' => 'controllers/error/404.php',
+];
+
+if (array_key_exists($_SERVER['REQUEST_URI'], $routes)) {
+    require(basePath($routes[$_SERVER['REQUEST_URI']]));
+} else {
+    require(basePath($routes['404']));
+}
